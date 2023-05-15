a531c1fcafd437bcf2e6fef25eafe86696c69c91
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-05-15 14:06:01 +0300

    Updated task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients')
+
+ingredients.forEach((ingredient) => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add('item');
+  ingredientsList.appendChild(ingredientEl);
+})
