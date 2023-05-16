cb1f438728708c729b791ea7be7b87ec517dc86f
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-05-16 16:27:03 +0300

    Updated task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const inputEmail = loginForm.elements.email;
+    const inputPassword = loginForm.elements.password;
+   
+    if (inputEmail.value === "" || inputPassword.value === "") {
+        alert("Будь-ласка, заповніть всі поля!!!");
+    }
+
+    const form = {
+        email: inputEmail.value,
+        password: inputPassword.value,
+    }
+
+    console.log(form)
+
+    loginForm.reset()
+});
 
