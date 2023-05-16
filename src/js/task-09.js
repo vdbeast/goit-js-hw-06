1a770c686b3a39b8a8ed01bc59f5564598a9d21f
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-05-16 16:48:04 +0300

    Updated task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widget = document.querySelector(".widget");
+const colorSpan = widget.querySelector('.color');
+const chandeColorBtn = widget.querySelector(".change-color");
+
+chandeColorBtn.addEventListener(('click'), () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+})
